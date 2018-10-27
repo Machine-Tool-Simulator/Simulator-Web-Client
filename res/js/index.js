31b1987914c0aa107c8e57997b8e306b17371a8c
TPeterW <peterwangtao0@hotmail.com>
2018-10-27 03:16:37 -0400

    Add number now works like a calculator

--- res/js/index.js
+++ res/js/index.js
@@ -1,20 +1,20 @@
 let videoCounter = 0;
 let selectedCoord = 0;
-let xbutton = getById("Xbutton");
-let zbutton = getById("Zbutton");
+let xbutton = getById('Xbutton');
+let zbutton = getById('Zbutton');
 
 /** Initialization */
 window.onload = function() {
-	xbutton.addEventListener("click", function() {
+	xbutton.addEventListener('click', function() {
 		selectedCoord = 1;
-		xbutton.style.backgroundColor = "rgb(0,0,0)";
-		zbutton.style.backgroundColor = "rgb(85,80,74)";
+		xbutton.style.backgroundColor = 'rgb(0,0,0)';
+		zbutton.style.backgroundColor = 'rgb(85,80,74)';
 	});
 	
-	zbutton.addEventListener("click", function() {
+	zbutton.addEventListener('click', function() {
 		selectedCoord = 2;
-		zbutton.style.backgroundColor = "rgb(0,0,0)";
-		xbutton.style.backgroundColor = "rgb(236,210,175)";
+		zbutton.style.backgroundColor = 'rgb(0,0,0)';
+		xbutton.style.backgroundColor = 'rgb(236,210,175)';
 	});
 }
 
@@ -24,7 +24,19 @@ function numberPressed(element){
 }
 
 function addNumber(current, digit) {
-	// TODO: actually add the calculator function
+	if (digit === '.' && current.indexOf(digit) >= 0) return current;
+	if (current === '0') {
+		if (digit === '0') return current;
+		if (digit === '.') return current + digit;
+		else return digit;
+	}
+	if (digit === '+/-') {
+		if (current.length <= 0) return current;
+		if (current.charAt(0) === '-') current = current.substring(1);
+		else current = '-' + current;
+		return current;
+	}
+
 	return current + digit;
 }
 
@@ -36,7 +48,7 @@ function setAbsPos() {
 
 	let buffer = getById('buffer');
 	targetVar.value = buffer.value;
-	buffer.value = "";
+	buffer.value = '';
 }
 
 function setIncPos() {
@@ -49,7 +61,7 @@ function setIncPos() {
 	if (buffer.value.length <= 0) return;
 	if (targetVar.value.length <= 0) targetVar.value = 0;
 	targetVar.value = parseFloat(targetVar.value) + parseFloat(buffer.value);
-	buffer.value = "";
+	buffer.value = '';
 }
 
 function restore() {
@@ -58,36 +70,36 @@ function restore() {
 	if (selectedCoord == 1) targetVar = getById('xvar');
 	else if (selectedCoord == 2) targetVar = getById('zvar');	// not using else here in case of other weird values
 
-	targetVar.value = "";
+	targetVar.value = '';
 }
 
 function switchVideo(element) {
 	videoCounter += 1;
-	let video_1 = getById("videoList_1");
-	let video_2 = getById("videoList_2");
-	let video_3 = getById("videoList_3");
-	let video_4 = getById("videoList_4");
-	let video_5 = getById("videoList_5");
-	let video_end = getById("videoList_end");
+	let video_1 = getById('videoList_1');
+	let video_2 = getById('videoList_2');
+	let video_3 = getById('videoList_3');
+	let video_4 = getById('videoList_4');
+	let video_5 = getById('videoList_5');
+	let video_end = getById('videoList_end');
 
 	if (videoCounter == 1) {
-		video_1.style.display = "block";
+		video_1.style.display = 'block';
 	} else if (videoCounter == 2) {
-		video_2.style.display = "block";
-		video_1.style.display = "none"
+		video_2.style.display = 'block';
+		video_1.style.display = 'none';
 	} else if (videoCounter == 3) {
-		video_3.style.display = "block";
-		video_2.style.display = "none"
+		video_3.style.display = 'block';
+		video_2.style.display = 'none';
 	} else if (videoCounter == 4) {
-		video_4.style.display = "block";
-		video_3.style.display = "none";
+		video_4.style.display = 'block';
+		video_3.style.display = 'none';
 	} else if (videoCounter == 5) {
-		video_5.style.display = "block";
-		video_4.style.display = "none";
+		video_5.style.display = 'block';
+		video_4.style.display = 'none';
 
 	} else if (videoCounter == 6) {
-		video_end.style.display = "block";
-		video_5.style.display = "none";
+		video_end.style.display = 'block';
+		video_5.style.display = 'none';
 	}
 }
 
