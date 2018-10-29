e651a21fb8c4a3eca2f3db96fa9bfb7cf33e2493
Junfei <Junfei.Yu.TH@dartmouth.edu>
2018-10-29 14:44:26 -0400

    timer added

--- res/js/index.js
+++ res/js/index.js
@@ -22,7 +22,7 @@ window.onload = function() {
         selectedCoord = 1;
         controlPressed("X");
 	});
-	
+
 	zbutton.addEventListener('click', function() {
         resetColors();
 		zbutton.style.backgroundColor = 'rgb(0,0,0)';
@@ -247,4 +247,28 @@ function negBuffer() {
 /** Helpers */
 function getById(id) {
 	return document.getElementById(id);
+}
+
+ // ** For the timer */
+
+document.getElementById('timer').innerHTML = 20 + ":" + 05;
+startTimer();
+
+function startTimer() {
+  var presentTime = document.getElementById('timer').innerHTML;
+  var timeArray = presentTime.split(/[:]+/);
+  var m = timeArray[0];
+  var s = checkSecond((timeArray[1] - 1));
+  if(s==59){m=m-1}
+  //if(m<0){alert('timer completed')}
+
+  document.getElementById('timer').innerHTML =
+    m + ":" + s;
+  setTimeout(startTimer, 1000);
+}
+
+function checkSecond(sec) {
+  if (sec < 10 && sec >= 0) {sec = "0" + sec}; // add zero in front of numbers < 10
+  if (sec < 0) {sec = "59"};
+  return sec;
 }
