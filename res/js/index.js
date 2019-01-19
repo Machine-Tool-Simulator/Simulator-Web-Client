ac315ff5a85f3ef806eb65a6785e3ba4863fca0f
TPeterW <peterwangtao0@hotmail.com>
2019-01-19 13:09:23 -0500

    Hid group2 from user

--- res/js/index.js
+++ res/js/index.js
@@ -1,11 +1,9 @@
 function jump() {
 	let id = document.getElementById('user-id').value;
 	console.log(id);
-	if (id % 3 == 0) {
+	if (id % 2 == 0) {
 		window.location.href = 'group1.html';
-	} else if (id % 3 == 1) {
-		window.location.href = 'group2.html';
-	} else { // id % 3 == 2
+	} else {
 		window.location.href = 'group3.html';
-	}
+	} 
 }
