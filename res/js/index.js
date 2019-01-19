7870454db50f2a74c2e3dcaded70b3e9f085de51
TPeterW <peterwangtao0@hotmail.com>
2019-01-19 13:11:19 -0500

    Revert "Hid group2 from user"
    This reverts commit ac315ff5a85f3ef806eb65a6785e3ba4863fca0f.

--- res/js/index.js
+++ res/js/index.js
@@ -1,9 +1,11 @@
 function jump() {
 	let id = document.getElementById('user-id').value;
 	console.log(id);
-	if (id % 2 == 0) {
+	if (id % 3 == 0) {
 		window.location.href = 'group1.html';
-	} else {
+	} else if (id % 3 == 1) {
+		window.location.href = 'group2.html';
+	} else { // id % 3 == 2
 		window.location.href = 'group3.html';
-	} 
+	}
 }
